6d82627f7623669dea26484e7cf0a0481818cb4c
Klaas Schoute <klaas_schoute@hotmail.com>
2020-07-11 00:23:12 +0200

    :truck: Big renaming challenge

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware('auth:api')->group(function () {
     Route::post('logout','Api\AuthController@logout');
+    
+    //project
     Route::post('projects/create','Api\ProjectsController@create');
+    Route::post('projects/delete','Api\ProjectsController@delete');
+    Route::post('projects/update','Api\ProjectsController@update');
+    Route::get('projects','Api\ProjectsController@projects');
+    Route::get('projects/my_projects','Api\ProjectsController@myProjects');
 });
-
 
 //user
 Route::post('login','Api\AuthController@login');
@@ -30,19 +35,11 @@ Route::post('refresh', 'Api\AuthController@refresh');
 // Route::get('logout','Api\AuthController@logout');
 Route::post('save_user_info','Api\AuthController@saveUserInfo')->middleware('jwtAuth');
 
-//project
-Route::post('projects/delete','Api\ProjectsController@delete')->middleware('jwtAuth');
-Route::post('projects/update','Api\ProjectsController@update')->middleware('jwtAuth');
-Route::get('projects','Api\ProjectsController@projects')->middleware('jwtAuth');
-Route::get('projects/my_projects','Api\ProjectsController@myProjects')->middleware('jwtAuth');
-
-
 //comment
 Route::post('comments/create','Api\CommentsController@create')->middleware('jwtAuth');
 Route::post('comments/delete','Api\CommentsController@delete')->middleware('jwtAuth');
 Route::post('comments/update','Api\CommentsController@update')->middleware('jwtAuth');
 Route::post('projects/comments','Api\CommentsController@comments')->middleware('jwtAuth');
-
 
 //like
 Route::post('projects/like','Api\LikesController@like')->middleware('jwtAuth');
