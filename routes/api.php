882f77589955073720d683d70cf338d3b6940770
Klaas Schoute <klaas_schoute@hotmail.com>
2020-08-18 23:34:45 +0200

    :hammer: Pas auth:api toe op user_user_info

--- routes/api.php
+++ routes/api.php
@@ -18,31 +18,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:api')->group(function () {
+    // User
+    Route::post('save_user_info','Api\AuthController@saveUserInfo');
     Route::post('logout','Api\AuthController@logout');
     
-    //project
+    // project
     Route::post('projects/create','Api\ProjectsController@create');
     Route::post('projects/delete','Api\ProjectsController@delete');
     Route::post('projects/update','Api\ProjectsController@update');
     Route::get('projects','Api\ProjectsController@projects');
     Route::get('projects/my_projects','Api\ProjectsController@myProjects');
+    
 });
 
 
-//user
+// user (without authentication)
 Route::post('login','Api\AuthController@login');
 Route::post('register','Api\AuthController@register');
 Route::post('refresh', 'Api\AuthController@refresh');
 // Route::get('logout','Api\AuthController@logout');
-Route::post('save_user_info','Api\AuthController@saveUserInfo')->middleware('jwtAuth');
-
 
 //comment
 Route::post('comments/create','Api\CommentsController@create')->middleware('jwtAuth');
 Route::post('comments/delete','Api\CommentsController@delete')->middleware('jwtAuth');
 Route::post('comments/update','Api\CommentsController@update')->middleware('jwtAuth');
 Route::post('projects/comments','Api\CommentsController@comments')->middleware('jwtAuth');
-
 
 //like
 Route::post('projects/like','Api\LikesController@like')->middleware('jwtAuth');
