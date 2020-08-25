2c8f46954fb594d49e5a4287df48dc1f03c1e56e
lars van der niet <larsvanderniet@gmail.com>
2020-08-25 19:42:40 +0000

    fixed typo

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -113,7 +113,7 @@ class AuthController extends Controller {
 
         return response()->json([
             'success' => true,
-            'message' => 'project edited'
+            'message' => 'profile edited'
         ]);
     }
 
@@ -178,6 +178,7 @@ class AuthController extends Controller {
         } else {
             // default image
             $user->photo = "user.png";
+            $photo = "user.png";
         }
 
         $user->update();
